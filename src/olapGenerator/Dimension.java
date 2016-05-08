34be94778b6bf71075d1c5b5d474fb41379db1ec
Sean Vail <seanv554@gmail.com>
2016-05-08 16:36:17 -0700

    Dimension RollUp and DrillDown implemented

--- src/olapGenerator/Dimension.java
+++ src/olapGenerator/Dimension.java
@@ -1,49 +1,122 @@
 package olapGenerator;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Dimension {
 
-	private String columnName;
 	private String tableName;
 	private String keyName;
 	private boolean isGrouped;
 	private Map<Integer, String> conceptHierarchy;
-	
-	public Dimension(String tableName, String columnName, String keyName){
-		this(tableName, columnName, keyName, false);
+	private int currentHierarchyLevel;
+
+	public Dimension(String tableName, String columnName, String keyName) {
+		this(tableName, columnName, keyName, true);
 	}
-	
-	public Dimension(String tableName, String columnName, String keyName, boolean isGrouped){
+
+	public Dimension(String tableName, String initialColumnName, String keyName, boolean isGrouped) {
 		this.tableName = tableName;
-		this.columnName = columnName;
 		this.keyName = keyName;
 		this.isGrouped = isGrouped;
 		this.conceptHierarchy = new TreeMap<>();
-	}
-	
-	public Map<Integer, String> getConceptHierarchy(){
-		return this.conceptHierarchy;
-	}
-	
-	public String getColumnName(){
-		return this.columnName;
+		currentHierarchyLevel = 0;
+		this.conceptHierarchy.put(currentHierarchyLevel, initialColumnName);
+		conceptHierarchy.get(currentHierarchyLevel);
 	}
 
-	public String getTableName(){
+	public Map<Integer, String> getConceptHierarchy() {
+		return this.conceptHierarchy;
+	}
+
+	public boolean addConceptLevel(int hierarchyLevel, String columnName) {
+		if (conceptHierarchy.containsKey(hierarchyLevel)) {
+			return false;
+		}
+		conceptHierarchy.put(hierarchyLevel, columnName);
+		return true;
+	}
+
+	public boolean rollUp() {
+		List<Integer> sortedKeys = conceptHierarchy.keySet().stream().sorted(new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				if (o1 < o2) {
+					return -1;
+				} else if (o1 > o2) {
+					return 1;
+				} else {
+					return 0;
+				}
+			}
+
+		}).collect(Collectors.toList());
+
+		Iterator<Integer> itr = sortedKeys.iterator();
+		while (itr.hasNext()) {
+			int i = itr.next();
+			if (i > currentHierarchyLevel) {
+				currentHierarchyLevel = i;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean drillDown() {
+		List<Integer> reverseSortedKeys = conceptHierarchy.keySet().stream().collect(Collectors.toList());
+
+		reverseSortedKeys.sort(new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				if (o1 < o2) {
+					return 1;
+				} else if (o1 > o2) {
+					return -1;
+				} else {
+					return 0;
+				}
+			}
+
+		});
+
+		Iterator<Integer> itr = reverseSortedKeys.iterator();
+		while (itr.hasNext()) {
+			int i = itr.next();
+			if (i < currentHierarchyLevel) {
+				currentHierarchyLevel = i;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public String getColumnName() {
+		return conceptHierarchy.get(currentHierarchyLevel);
+	}
+
+	public String getTableName() {
 		return this.tableName;
 	}
 
-	public String getKeyName(){
+	public String getKeyName() {
 		return this.keyName;
 	}
 
 	public boolean isGrouped() {
 		return this.isGrouped;
 	}
-	
-	public void setGrouped(boolean isGrouped){
+
+	public void setGrouped(boolean isGrouped) {
 		this.isGrouped = isGrouped;
 	}
 }
